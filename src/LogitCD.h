800dfa5913c4e753fe63ac9ffdc6e75127524599
Cen Wu <30509304+cenwu@users.noreply.github.com>
2021-02-27 23:12:12 -0600

    Update LogitCD.h

--- src/LogitCD.h
+++ src/LogitCD.h
@@ -9,7 +9,7 @@ using namespace arma;
 
 arma::vec Network(arma::mat const &x, arma::vec const &y, double lam1, double lam2, arma::vec b, double r, arma::mat const &a, arma::vec const &u, int n, int p);
 
-arma::vec Network(const arma::mat& x, const arma::vec& y, double lam1, double lam2, arma::vec b, double r, const arma::mat& a, int n, int p);
+arma::vec Network(const arma::mat& x, const arma::vec& y, double lam1, double lam2, arma::vec b, double r, const arma::mat& a, int n, int p); //?? difference
 
 arma::vec MCP(arma::mat const &x, arma::vec const &y, double lambda, arma::vec b, double r, int n, int p);
 
